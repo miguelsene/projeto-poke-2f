245c0cf10e98ebc536859e1f39a9b6e07cbe8c35
miguelsene <rm95370@estudante.fieb.edu.br>
2025-05-23 12:52:14 +0000

    pokemon 3

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,16 +9,7 @@ const Home =()=>{
             <img src={eevee} />
              <img src={poke} />
             <img src={eevee} />
-             <img src={poke} />
-            <img src={eevee} />
-    <img src={poke} />
-            <img src={eevee} />
-
-             <img src={poke} />
-            <img src={eevee} />
-             <img src={poke} />
-            <img src={eevee} />
-
+        
         </div>
     )
 }
