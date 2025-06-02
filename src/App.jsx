0ee32b8ba65aad146b69047bc713e2690b5f34af
miguelsene <rm95370@estudante.fieb.edu.br>
2025-06-02 12:10:57 +0000

    pokemon 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-
+import PokeGrid from "./PokeGrid";
+import Pokeflex from "./Pokeflex";
 const App =() => {
   return(
     <Routes>
+      <Route path="pokegrid" element={<PokeGrid/>}/>
+     <Route path="pokeflex" element={<Pokeflex/>}/>
+      
+    
 <Route path="/" element = {<Home/>} />
+
     </Routes>
   )
 }
